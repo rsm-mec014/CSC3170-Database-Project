8e0d0e940396a061bbead29781244cb980899ada
wuli-mA <119010020@link.cuhk.edu.cn>
2022-12-22 16:49:00 +0800

    revised data generation

--- web/pagelib/staff/test.sql
+++ web/pagelib/staff/test.sql
@@ -1,11 +1,6 @@
 -- Active: 1668517554814@@127.0.0.1@3306@project
-select u.province,c.chip_version,sum(c.price) 
-from user AS u, package AS p, chip AS c
-where u.user_id=p.user_id
-group by c.chip_name;
-select c.chip_name as ChipName,sum(p.budget) as revenue
-from user AS u, package AS p, chip AS c
-where u.user_id=p.user_id and province='Guangdong'
+select u.province, c.chip_name as ChipName,sum(p.budget) as revenue
+from user AS u natural join package AS p, chip AS c
+where p.package_id=c.package_id and province in ('Guangdong','Hubei')
 group by c.chip_name
-order by sum(p.budget) desc
-limit 0,5;
+order by sum(p.budget) desc;
